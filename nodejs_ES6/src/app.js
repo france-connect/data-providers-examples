d2b372728d6f40ac4537508c80711ba700ab2b32
Raphaël Dubigny <rdubigny@gmail.com>
2018-08-28 11:14:18 +0200

    fix production installation

--- nodejs_ES6/src/app.js
+++ nodejs_ES6/src/app.js
@@ -6,7 +6,7 @@ import { initializeMock } from '../mock/france-connect';
 import getDgfipData from './controllers';
 import { fcHost } from '../config/config';
 
-if (process.env.LOCAL_LOOP !== 'false') {
+if (process.env.NODE_ENV !== 'production' && process.env.LOCAL_LOOP !== 'false') {
   initializeMock();
 } else {
   console.log('\x1b[31m%s\x1b[0m', `Remote loop mode activated: this server will hit ${fcHost}`); // eslint-disable-line no-console
